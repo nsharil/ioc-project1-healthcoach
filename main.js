34566a691068ebc2b804e8c5f72c54e5710411c2
nsharil <n.sharil@gmail.com>
2021-11-16 12:00:45 +0800

    Slider Edit
    Addes JQuery for Sleek Slider

---
+++ main.js
@@ -0,0 +1,12 @@
+$('.slider-container').slick({
+	autoplay: true, // Do we want it to autoplay? true or false
+	autoplaySpeed: 2000, // How long between each slide when auto-playing
+	speed: 500, // How fast is the transition in milliseconds
+	arrows: true, // Do you want to show arrows to trigger each slide
+	accessibility: true, // Enables keyboard tabbing and arrow key navigation
+	dots: true, // Enables the dots below to show how many slides
+	fade: false, // Changes the animate from slide to fade if true
+	infinite: false, // When true, means that it will scroll in a circle
+	pauseOnHover: false, // When true means the autoplay pauses when hovering
+	pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+});

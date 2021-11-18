d2c03f6e2ba316d473dcb645a1d71f513ab1f6b0
nsharil <n.sharil@gmail.com>
2021-11-18 14:31:17 +0800

    Testimonial Update

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-
-	$('.slider-container').slick({
+$('.slider-container').slick({
 	autoplay: true, // Do we want it to autoplay? true or false
 	autoplaySpeed: 2000, // How long between each slide when auto-playing
 	speed: 500, // How fast is the transition in milliseconds
@@ -11,5 +9,4 @@ $(document).ready(function(){
 	infinite: false, // When true, means that it will scroll in a circle
 	pauseOnHover: false, // When true means the autoplay pauses when hovering
 	pauseOnDotsHover: true // Pauses the autoplay when hovering over the dots
-	});
 });

f2fee01c18ba88dee568d54d56e973de70363d99
nsharil <n.sharil@gmail.com>
2021-11-18 14:37:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ $('.slider-container').slick({
 	autoplay: true, // Do we want it to autoplay? true or false
 	autoplaySpeed: 2000, // How long between each slide when auto-playing
 	speed: 500, // How fast is the transition in milliseconds
-	arrows: true, // Do you want to show arrows to trigger each slide
+	arrows: true // Do you want to show arrows to trigger each slide
 });
